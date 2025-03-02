d0a8881fc4350f96f56852ee82064e051d669e7f
LuisAAO2694 <entlaao2611@gmail.com>
2025-03-02 16:32:55 -0600

    frontend a produccionv1

--- frontend/app.py
+++ frontend/app.py
@@ -6,30 +6,30 @@ app = Flask(__name__)
 API_URL = "http://127.0.0.1:8000"  # URL del backend FastAPI
 
 @app.route("/")
-def menu():
-    return render_template("index.html")  # Menú principal
-
-@app.route("/ping")
-def ping():
-    response = requests.get(f"{API_URL}/ping")
-    data = response.json()
-    return render_template("ping.html", message=data["message"])
-
-@app.route("/boton")
-def boton():
-    response = requests.get(f"{API_URL}/contador")
-    data = response.json()
-    return render_template("boton.html", contador=data["contador"])
-
-@app.route("/incrementar", methods=["POST"])
-def incrementar():
-    response = requests.post(f"{API_URL}/incrementar")
-    data = response.json()
-    return jsonify(data)  # Devuelve el nuevo contador en JSON
-
-@app.route("/front")
 def front():
     return render_template("front.html")  # Menú principal
+
+# @app.route("/ping")
+# def ping():
+#     response = requests.get(f"{API_URL}/ping")
+#     data = response.json()
+#     return render_template("ping.html", message=data["message"])
+
+# @app.route("/boton")
+# def boton():
+#     response = requests.get(f"{API_URL}/contador")
+#     data = response.json()
+#     return render_template("boton.html", contador=data["contador"])
+
+# @app.route("/incrementar", methods=["POST"])
+# def incrementar():
+#     response = requests.post(f"{API_URL}/incrementar")
+#     data = response.json()
+#     return jsonify(data)  # Devuelve el nuevo contador en JSON
+
+# @app.route("/front")
+# def front():
+#     return render_template("front.html")  # Menú principal
     
 
 if __name__ == "__main__":
